54f89508a086ca51f97e18b2b3760c59a9ea4795
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-04 12:19:35 +0530

    this is the new commit1456

--- main.tf
+++ main.tf
@@ -13,3 +13,24 @@ resource "aws_instance" "web_server" {
   }
 }
 
+resource "aws_security_group" "ssh_access" {
+  name        = "allow-ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP for security
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
