b669620579a811bbc8c70b25acd09ce027e52154
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-05 16:37:38 +0530

    this is the new commit753159

--- main.tf
+++ main.tf
@@ -7,28 +7,41 @@ provider "aws" {
 
 provider "aws" {
   alias      = "destination"
-  region     = "us-east-1"
+  region     = "ap-south-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 }
 
+# Generate random suffix for uniqueness
 resource "random_id" "suffix" {
   byte_length = 4
 }
 
+# Share AMI from Account A to Account B
+resource "aws_ami_launch_permission" "share_to_destination" {
+  provider    = aws.source
+  image_id    = var.ami_id
+  account_id  = var.destination_account_id
+}
+
+# Copy the shared AMI into destination account
 resource "aws_ami_copy" "copied_ami" {
   provider          = aws.destination
   name              = "copied-ami-${formatdate("YYYYMMDDhhmmss", timestamp())}"
   description       = "AMI copied from ap-south-1"
   source_ami_id     = var.ami_id
-  source_ami_region = "ap-south-1"
+  source_ami_region = "ap-south-2"
+
+  depends_on = [aws_ami_launch_permission.share_to_destination]
 }
 
+# Sleep for some time to allow the AMI to be available
 resource "time_sleep" "wait_for_ami" {
   depends_on      = [aws_ami_copy.copied_ami]
-  create_duration = "200s" # Wait for about 3.3 minutes
+  create_duration = "200s" # Wait for ~3.3 minutes
 }
 
+# Create a security group in destination account
 resource "aws_security_group" "ssh_access" {
   provider    = aws.destination
   name        = "allow-ssh-${random_id.suffix.hex}"
@@ -39,7 +52,7 @@ resource "aws_security_group" "ssh_access" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Replace with your IP for security in production
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -50,24 +63,15 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
+# Launch EC2 instance in destination using copied AMI
 resource "aws_instance" "web_server" {
   provider                    = aws.destination
   ami                         = aws_ami_copy.copied_ami.id
-  instance_type               = "t2.micro"
-  key_name                    = "newacc"
+  instance_type               = "t3.micro"
+  key_name                    = "newacc"  # Make sure this key exists in destination
   vpc_security_group_ids      = [aws_security_group.ssh_access.id]
   associate_public_ip_address = true
-
-  depends_on = [time_sleep.wait_for_ami]
-
-user_data = <<-EOF
-              #!/bin/bash
-              apt-get update -y
-              apt-get install -y apache2
-              systemctl start apache2
-              systemctl enable apache2
-              echo "This is the same AMI and Your data exists" > /var/www/html/index.html
-              EOF
+  depends_on                  = [time_sleep.wait_for_ami]
 
   tags = {
     Name = "MyRecreatedEC2"
--- variables.tf
+++ variables.tf
@@ -1,3 +1,4 @@
 variable "ami_id" {description = "The source AMI ID from ap-south-1"}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "destination_account_id" {}  # AWS Account ID of destination
