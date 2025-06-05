70a681728a05d7be8a7ed4c35de4d561a452a4e2
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-05 17:14:01 +0530

    this is the new commit75852

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Source Account A Provider
 provider "aws" {
   alias      = "source"
   region     = "ap-south-1"
@@ -5,11 +6,12 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+# Destination Account B Provider
 provider "aws" {
   alias      = "destination"
   region     = "ap-south-2"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  access_key = var.destination_access_key
+  secret_key = var.destination_secret_key
 }
 
 # Generate random suffix for uniqueness
@@ -19,9 +21,9 @@ resource "random_id" "suffix" {
 
 # Share AMI from Account A to Account B
 resource "aws_ami_launch_permission" "share_to_destination" {
-  provider    = aws.source
-  image_id    = var.ami_id
-  account_id  = var.destination_account_id
+  provider   = aws.source
+  image_id   = var.ami_id
+  account_id = var.destination_account_id
 }
 
 # Copy the shared AMI into destination account
@@ -30,18 +32,18 @@ resource "aws_ami_copy" "copied_ami" {
   name              = "copied-ami-${formatdate("YYYYMMDDhhmmss", timestamp())}"
   description       = "AMI copied from ap-south-1"
   source_ami_id     = var.ami_id
-  source_ami_region = "ap-south-2"
+  source_ami_region = "ap-south-1"  # ✅ fixed: must match the region of the source AMI
 
   depends_on = [aws_ami_launch_permission.share_to_destination]
 }
 
-# Sleep for some time to allow the AMI to be available
+# Sleep to wait for AMI to be fully available
 resource "time_sleep" "wait_for_ami" {
   depends_on      = [aws_ami_copy.copied_ami]
-  create_duration = "200s" # Wait for ~3.3 minutes
+  create_duration = "200s"
 }
 
-# Create a security group in destination account
+# Security group in destination account
 resource "aws_security_group" "ssh_access" {
   provider    = aws.destination
   name        = "allow-ssh-${random_id.suffix.hex}"
@@ -63,12 +65,12 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
-# Launch EC2 instance in destination using copied AMI
+# Launch EC2 in destination account using copied AMI
 resource "aws_instance" "web_server" {
   provider                    = aws.destination
   ami                         = aws_ami_copy.copied_ami.id
   instance_type               = "t3.micro"
-  key_name                    = "newacc"  # Make sure this key exists in destination
+  key_name                    = "newacc"  # ✅ Make sure this key exists in destination account!
   vpc_security_group_ids      = [aws_security_group.ssh_access.id]
   associate_public_ip_address = true
   depends_on                  = [time_sleep.wait_for_ami]
@@ -77,4 +79,3 @@ resource "aws_instance" "web_server" {
     Name = "MyRecreatedEC2"
   }
 }
-
