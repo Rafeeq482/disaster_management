d98648b43d2ee4c2d9289c24e21a71de8aead15f
Rafeeq482 <mohdrafeeqkhan14@gmail.com>
2025-06-05 17:17:54 +0530

    this is the new commit753159

--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "ami_id" {description = "The source AMI ID from ap-south-1"}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "destination_account_id" {}  # AWS Account ID of destination
+variable "destination_access_key" {}
+variable "destination_secret_key" {}
